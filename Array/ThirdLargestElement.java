044bac4580deed6e9bdd7699f0400580be1247ac
Ayushkumar8969 <103050632+Ayushkumar8969@users.noreply.github.com>
2022-06-26 12:21:37 +0530

    Update ThirdLargestElement.java

--- Array/ThirdLargestElement.java
+++ Array/ThirdLargestElement.java
@@ -1,55 +1,55 @@
 class ThirdLargestElement
 {
-    public static int element(int [] a,int n)
-    {
-        int l=0;
-        int sl=0;
-        int thirdLargest=-1;
-        int temp=0;
-        for(int i=1;i<n;i++)
-        {
-            if(a[i]>a[l])
-            {
-                temp=sl;
-                sl=l;
-                l=i;
-                if(a[sl]>a[temp] && thirdLargest<a[sl])
-                {
-                    thirdLargest=a[temp];
-                }
-            }
-            else
-            {
-                if(a[i]<a[l])
-                {
-                     if(sl==0 || a[sl]<a[i])
-                    {
-                      temp=sl;
-                      sl=i;
-                      if(a[sl]>a[temp] && thirdLargest<a[sl])
-                      {
-                        thirdLargest=a[temp];
+                        public static int element(int [] a,int n)
+                       {
+                                 int l=0;
+                                 int sl=0;
+                                 int thirdLargest=-1;
+                                 int temp=0;
+                                  for(int i=1;i<n;i++)
+                                  {
+                                             if(a[i]>a[l])
+                                             {
+                                                    temp=sl;
+                                                     sl=l;
+                                                       l=i;
+                                                          if(a[sl]>a[temp] && thirdLargest<a[sl])
+                                                          {
+                                                                 thirdLargest=a[temp];
+                                                          }
+                                              }
+                                               else
+                                              {
+                                                          if(a[i]<a[l])
+                                                          {
+                                                                      if(sl==0 || a[sl]<a[i])
+                                                                      {
+                                                                               temp=sl;
+                                                                                sl=i;
+                                                                                  if(a[sl]>a[temp] && thirdLargest<a[sl])
+                                                                                  {
+                                                                                            thirdLargest=a[temp];
+                                                                                   }
+                                                                        }
+                                                                         else{
+                                                                                 if(a[i]<a[sl])
+                                                                                  {
+                                                                                            if(a[i]>thirdLargest)
+                                                                                            {
+                                                                                                      thirdLargest=a[i];
+                                                                                             }
+                                                                                   }
+                                                                             }
+                                                             }
+                                                 }
+                                     }
+                            return thirdLargest;
                       }
-                    }
-                    else{
-                        if(a[i]<a[sl])
-                        {
-                           if(a[i]>thirdLargest)
-                           {
-                              thirdLargest=a[i];
-                           }
+                        public static void main(String[] args)
+                       {
+                               int[] a={10,20,50,40,30,15};
+                                int n=a.length;
+                                 int ans=element(a,n);
+                              System.out.println(ans);
                         }
-                    }
-               }
-            }
-        }
-        return thirdLargest;
-    }
-    public static void main(String[] args)
-    {
-        int[] a={10,20,50,40,30,15};
-        int n=a.length;
-        int ans=element(a,n);
-        System.out.println(ans);
-    }
 }
