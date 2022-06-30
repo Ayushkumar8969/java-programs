5b120316e0004fb6e12b018b2128fb388247e3ab
Ayushkumar8969 <103050632+Ayushkumar8969@users.noreply.github.com>
2022-06-30 18:45:36 +0530

    Update RightGreatestElement.java

--- Array/RightGreatestElement.java
+++ Array/RightGreatestElement.java
@@ -1,33 +1,30 @@
 class RightGreatestElement
 {
-    public static void rightSideGreatestElement(int[] a,int n)
-    {
-        int i;
-        for(i=0;i<n;i++)
-        {
-           int max=0;
-           for(int j=i+1;j<n;j++)
-           {
-                if(a[j]>max)
-                {
-                   max=a[j];
-                   a[i]=max;
-                }
-           }
-        }
-           if(i==n)
-           {
-              a[n-1]=0;
-           }
-        for(i=0;i<n;i++)
-        {
-            System.out.print(a[i]+" ");
-        }
-    }
-    public static void main(String[] args)
-    {
-        int[] a={10,20,50,12,15,5};
-        int n=a.length;
-        rightSideGreatestElement(a,n);
-    }
+                                        public static void rightSideGreatestElement(int[] a,int n)
+                                        {
+                                                    int i;
+                                                    for(i=0;i<n;i++)
+                                                    {
+                                                           int max=0;
+                                                             for(int j=i+1;j<n;j++)
+                                                             {
+                                                                    if(a[j]>max)
+                                                                    {
+                                                                          max=a[j];
+                                                                     }
+                                                              }
+                                                             a[i]=max;
+                                                    }
+                                                       a[n-1]=0;
+                                                           for(i=0;i<n;i++)
+                                                           {
+                                                                 System.out.print(a[i]+" ");
+                                                            }
+                                        }
+                                            public static void main(String[] args)
+                                            {
+                                                     int[] a={7,5,8,9,6,8,5,7,4,6};
+                                                        int n=a.length;
+                                                          rightSideGreatestElement(a,n);
+                                             }
 }
