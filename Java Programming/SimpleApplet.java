40ff22025777efbabd3c1aa351c5861821dd13b6
Ayushkumar8969 <103050632+Ayushkumar8969@users.noreply.github.com>
2022-07-24 10:43:28 +0530

    Add files via upload

---
+++ Java Programming/SimpleApplet.java
@@ -0,0 +1,13 @@
+// first applet program
+import java.applet.*;
+import java.awt.*;
+/* <applet code="SimpleApplet" width=500 height=500> 
+   </applet>
+*/ 
+public class SimpleApplet extends Applet 
+{
+                             public void paint() 
+                             { 
+                                     g.drawString("applets first program",450,300);
+                             }
+}
