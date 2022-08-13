136ef07949d8e679df01e5a87d94924fad13e063
Ayushkumar8969 <103050632+Ayushkumar8969@users.noreply.github.com>
2022-08-13 23:39:16 +0530

    Add files via upload

---
+++ Searching/CountEvenNumberOfDigits.java
@@ -0,0 +1,36 @@
+class CountEvenNumberOfDigits 
+{
+                             static int countEvenNumberOfDigits(int[] arr) 
+                             {
+                                                 int count=0;
+                                                    if(arr.length==0) 
+                                                    {
+                                                            return Integer.MIN_VALUE;
+                                                    }
+                                                      for(int i=0;i<arr.length;i++) 
+                                                      {
+                                                                   int count1=0;
+                                                                   int n=arr[i];
+                                                                      /*  while(n!=0) 
+                                                                        {
+                                                                               count1++;
+                                                                               n=n/10;
+                                                                        }*/
+                                                                        if(n<0)
+                                                                        {
+                                                                            n=n*-1; 
+                                                                        }
+                                                                        count1=(int)(Math.log10(n))+1;
+                                                                        if(count1%2==0) 
+                                                                        {
+                                                                               count++;
+                                                                        }
+                                                      }
+                                                        return count;
+                             } 
+                                public static void main(String[] args) 
+                                {
+                                                  int[] arr={15,65,20,3,789,4562,15235,82,-60};
+                                                  System.out.print(countEvenNumberOfDigits(arr));
+                                }
+}
