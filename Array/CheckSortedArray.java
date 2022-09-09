84b59d674fed2a5f4a2401b332a3078558c48fbf
Ayushkumar8969 <103050632+Ayushkumar8969@users.noreply.github.com>
2022-09-09 22:36:07 +0530

    Update CheckSortedArray.java

--- Array/CheckSortedArray.java
+++ Array/CheckSortedArray.java
@@ -1,32 +1,19 @@
-public class CheckSortedArray
+class CheckSorted 
 {
-                                 public static int checkArray(int[] a)
-                                 {
-                                        int n=a.length;
-                                         int j=0;
-                                             for(int i=1;i<n;i++)
-                                             {
-                                                  if(a[i]>=a[j])
-                                                  {
-                                                         j++;
-                                                   }
-                                                   else
-                                                     {
-                                                         return 0;
-                                                     }
-                                             }
-                                                return 1;
-                                 }
-                                     public static void main(String[] args)
+                        static boolean arraySortedOrNot(int[] a)
+                        {
+                                     for(int i=0;i<a.length-1;i++)
                                      {
-                                            int[] a={15,30,45,60,75};
-                                             int res=checkArray(a);
-                                                if(res==1)
-                                                {
-                                                    System.out.println("Sorted Array");
-                                                 }
-                                                else{
-                                                    System.out.println("Unsorted Array");
-                                                   }
-                                       }
+                                              if(a[i]>a[i+1])
+                                              {  
+                                                       return false;
+                                              }
+                                     }
+                                       return true;
+                        }
+                             public static void main(String[] args)
+                             {
+                                             int[] a={10,25,25,30,80,95};
+                                             System.out.print(arraySortedOrNot(a));
+                             }
 }
