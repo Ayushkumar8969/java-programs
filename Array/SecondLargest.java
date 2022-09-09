a75783fb1f7f96edf0d5e44cc408041a4e902de4
Ayushkumar8969 <103050632+Ayushkumar8969@users.noreply.github.com>
2022-09-09 23:18:56 +0530

    Update SecondLargest.java

--- Array/SecondLargest.java
+++ Array/SecondLargest.java
@@ -1,39 +1,39 @@
 class SecondLargest
 {
-    public static int secondLargest(int[] a,int n)
-    {
-        int largest=0;
-        int secondLargest=-1;
-          if(n<2)
-          {
-            return -1;
-          }
-          for(int i=1;i<n;i++)
-          {
+                        public static int secondLargest(int[] a)
+                        {
+                                          int largest=0;
+                                            int secondLargest=-1;
+                                           if(a.length<2)
+                                           {
+                                                 return -1;
+                                           }
+                                               for(int i=1;i<a.length;i++)
+                                               {
            
-               if(a[i]>a[largest])
-              {
-                 secondLargest=largest;
-                 largest=i;
-              }
-              else
-              {
-                     if(a[i]<a[largest])
-                     {
-                           if(secondLargest==-1 || a[i]>a[secondLargest])
-                           {
-                               secondLargest=i;
-                           }
-                     }
-              }
-          }
-          return secondLargest;
-    }
-        public static void main(String[] args)
-        {
-            int[] a={10,15,50,40,20,5};
-            int n=a.length;
-            int secondMaxIndex=secondLargest(a,n);
-            System.out.print(a[secondMaxIndex]);
-        }
+                                                              if(a[i]>a[largest])
+                                                              {
+                                                                     secondLargest=largest;
+                                                                      largest=i;
+                                                              }
+                                                              else
+                                                              {
+                                                                           if(a[i]<a[largest])
+                                                                           {
+                                                                                   if(secondLargest==-1 || a[i]>a[secondLargest])
+                                                                                   {
+                                                                                               secondLargest=i;
+                                                                                    }
+                                                                            }
+                                                               }
+                                                 }
+                                             return secondLargest;
+                             }
+                                              public static void main(String[] args)
+                                              {
+                                                      int[] a={10,15,50,40,20,5};
+                                                      int n=a.length;
+                                                       //int secondMaxIndex=secondLargest(a);
+                                                          System.out.print(a[secondLargest(a)]);
+                                               }
 }
