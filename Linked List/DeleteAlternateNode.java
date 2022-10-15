4d655ca76f79b01d3023330e00ec425a2d769f19
Ayushkumar8969 <103050632+Ayushkumar8969@users.noreply.github.com>
2022-10-15 12:22:48 +0530

    Add files via upload

---
+++ Linked List/DeleteAlternateNode.java
@@ -0,0 +1,55 @@
+class LinkedList 
+{
+                                 Node head;
+                                 class Node 
+                                 {
+                                             int data;
+                                             Node next;
+                                                Node(int data)
+                                                {
+                                                        this.data=data;
+                                                }
+                                 }
+                                 public void display()
+                                 {
+                                               Node temp=head;
+                                                   while(temp!=null)
+                                                   {
+                                                                  System.out.print(temp.data+"->");
+                                                                  temp=temp.next;
+                                                   }
+                                                   System.out.println("END");
+                                 }
+                                 public void insertAtFirst(int data)
+                                 {
+                                                            Node nn=new Node(data);
+                                                              nn.next=head;
+                                                              head=nn;
+                                 }
+                                 public void deleteAlternate(Node head)
+                                 {
+                                                     Node temp=head;
+                                                      while(temp!=null && temp.next!=null)
+                                                      {
+                                                                    temp.next=temp.next.next;
+                                                                     temp=temp.next;
+                                                      }
+                                 }
+}
+class DeleteAlternateNode
+{
+                                                    public static void main(String[] args)
+                                                    {
+                                                                  LinkedList list=new LinkedList();
+                                                                      list.insertAtFirst(65);
+                                                                      list.insertAtFirst(25);
+                                                                      list.insertAtFirst(35);
+                                                                      list.insertAtFirst(15);
+                                                                      list.insertAtFirst(95);
+                                                                      list.display();
+                                    
+                                                                    list.deleteAlternate(list.head);
+                                                                       list.display();
+
+                                                    }
+}
