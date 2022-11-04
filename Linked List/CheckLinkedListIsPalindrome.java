484c2f9d02c0fc537a6e51d2da258e33b52ec00e
Ayushkumar8969 <103050632+Ayushkumar8969@users.noreply.github.com>
2022-11-04 09:23:35 +0530

    Add files via upload

---
+++ Linked List/CheckLinkedListIsPalindrome.java
@@ -0,0 +1,93 @@
+class LL 
+{
+                                  Node head;
+                                  class Node
+                                  {
+                                                   int data;
+                                                   Node next;
+                                                     Node(int data)
+                                                     {
+                                                               this.data=data;
+                                                     }
+                                  }
+                                  public void display()
+                                  {
+                                                    Node temp=head;
+                                                    while(temp!=null)
+                                                    {
+                                                              System.out.print(temp.data+"->");
+                                                              temp=temp.next;
+                                                    }
+                                                    System.out.println("END");
+                                  }
+                                  public void insertAtFirst(int data)
+                                  {
+                                                 Node nn=new Node(data);
+                                                 nn.next=head;
+                                                 head=nn;
+                                  }
+                                  public boolean checkPalindrome(Node head)
+                                  {
+                                                       Node node=null;
+                                                       Node slow=head;
+                                                       Node fast=head;
+                                                       while(fast!=null && fast.next!=null)
+                                                       {
+                                                                     slow=slow.next;
+                                                                     fast=fast.next.next;
+                                                       }
+                                                       if(fast==null) //Even case
+                                                       {
+                                                                    node=reverse(slow);
+                                                       }
+                                                       else       // Odd case
+                                                       {
+                                                                  node=reverse(slow.next);
+                                                       }
+                                                       return check(head,node);
+                                  }
+                                  public Node reverse(Node head)
+                                  {
+                                                 Node prev=null,curr=head,temp=null;
+                                                 while(curr!=null)
+                                                 {
+                                                            temp=curr.next;
+                                                            curr.next=prev;
+                                                            prev=curr;
+                                                            curr=temp;
+                                                 }
+                                                 return prev;
+                                  }
+                                  public boolean check(Node head,Node node)
+                                  {
+                                                          while(node!=null)
+                                                          {
+                                                                       if(head.data==node.data)
+                                                                       {
+                                                                              head=head.next;
+                                                                              node=node.next;
+                                                                       }
+                                                                       else 
+                                                                       {
+                                                                               return false;
+                                                                       }
+                                                          }
+                                                          return true;
+                                  }
+
+}
+class CheckLinkedListIsPalindrome
+ {
+                               public static void main(String[] args)
+                                {
+                                                       LL list=new LL();
+                                                         list.insertAtFirst(10);
+                                                         list.insertAtFirst(20);
+                                                         list.insertAtFirst(30);
+                                                         list.insertAtFirst(30);
+                                                         list.insertAtFirst(20);
+                                                         list.insertAtFirst(10);
+                                                         list.display();
+                                                         System.out.println(list.checkPalindrome(list.head));
+                               }
+}
