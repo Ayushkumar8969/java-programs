0560b8c8347e01e38948ae55afb529dec1d0578b
Ayushkumar8969 <103050632+Ayushkumar8969@users.noreply.github.com>
2022-11-05 23:06:02 +0530

    Add files via upload

---
+++ Linked List/AddOneToLinkedList1.java
@@ -0,0 +1,90 @@
+class LL 
+{
+                           Node head;
+                           class Node
+                           {
+                                    int data;
+                                    Node next;
+                                      Node(int data)
+                                      {
+                                               this.data=data;
+                                      }
+                           }
+                           public void display()
+                            {
+                                       Node temp=head;
+                                       while(temp!=null)   
+                                       {
+                                                 System.out.print(temp.data+"->");
+                                                  temp=temp.next;
+                                       }  
+                                       System.out.println("END");        
+                            }
+                            public void insertAtFirst(int data)
+                            {
+                                         Node nn=new Node(data);
+                                         nn.next=head;
+                                         head=nn;
+                            }
+                            public Node addOne(Node head)
+                            {
+                                             head=reverse(head);
+                                             Node temp=head;
+                                             Node prev=null;
+                                             int carry=1;
+                                             while(temp!=null)
+                                             {
+                                                           int value=temp.data+carry;
+                                                           if(value%10==0)
+                                                           {
+                                                                    temp.data=value%10;
+                                                                    carry=value/10;
+                                                           }
+                                                           else 
+                                                           {
+                                                                     temp.data=value%10;
+                                                                     carry=value/10;
+                                                                         break;
+                                                           }
+                                                           prev=temp;
+                                                           temp=temp.next;
+                                             }
+                                             if(carry!=0)
+                                             {
+                                                        Node nn=new Node(carry);
+                                                            prev.next=nn;
+                                             }
+                                             return reverse(head);
+                            }
+                            public Node reverse(Node head)
+                            {
+                                                Node prev=null,curr=head,temp=null;
+                                                   while(curr!=null)
+                                                   {
+                                                              temp=curr.next;
+                                                              curr.next=prev;
+                                                              prev=curr;
+                                                              curr=temp;
+                                                   }
+                                                   return prev;
+                            }
+
+}
+class AddOneToLinkedList1
+{
+                                               public static void main(String[] args)
+                                               {
+                                                                  LL list=new LL();
+                                                                   list.insertAtFirst(9);
+                                                                   list.insertAtFirst(9);
+                                                                   list.insertAtFirst(9);
+                                                                   list.insertAtFirst(9);
+                                                                   list.insertAtFirst(4);
+                                                                   list.display();
+                                                                   list.head=list.addOne(list.head);
+                                                                   list.display();
+
+
+                                               }
+}
+
