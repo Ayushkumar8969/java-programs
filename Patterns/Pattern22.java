41ad1c9ce907975246f22fcbfe4308154cd606da
Ayushkumar8969 <103050632+Ayushkumar8969@users.noreply.github.com>
2022-12-14 18:34:51 +0530

    Add files via upload

---
+++ Patterns/Pattern22.java
@@ -0,0 +1,15 @@
+public class Pattern22 {
+                    public static void main(String[] args)
+                    {
+                             int x=1;
+                            for(int row=1;row<=5;row++)
+                            {
+                                    for(int col=1;col<=row;col++)
+                                    {
+                                           System.out.print(x+" ");
+                                           x++;
+                                    }
+                                    System.out.println();
+                            }
+                    }
+}
