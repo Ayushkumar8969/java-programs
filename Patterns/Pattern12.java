c3b78bf2e0fed04dde5c486baec735a2dc77f5bb
Ayushkumar8969 <103050632+Ayushkumar8969@users.noreply.github.com>
2022-12-17 12:27:14 +0530

    Add files via upload

---
+++ Patterns/Pattern12.java
@@ -0,0 +1,33 @@
+public class Pattern12 {
+                        public static void main(String[] args) 
+                        {
+                                for(int row=1;row<=9;row++)
+                                {
+                                      boolean flag=true;
+                                        for(int col=1;col<=9;col++)
+                                        {
+                                              if((col>=row && col<=10-row && flag) || (col>=10-row && col<=row && flag))
+                                              {
+                                                   System.out.print("*");
+                                                   flag=false;
+                                              }
+                                              else{
+                                                   System.out.print(" ");
+                                                   flag=true;
+                                              }
+                                        }
+                                        System.out.println();
+                                }
+                        }
+                        /*    
+                                         * * * * *
+                                          * * * * 
+                                           * * *  
+                                            * *   
+                                             *    
+                                            * *   
+                                           * * *  
+                                          * * * * 
+                                         * * * * *
+                         */
+}
